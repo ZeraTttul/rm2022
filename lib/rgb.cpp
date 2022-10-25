c2435e39e4447da69b3d8796f757099062e81029
ZeraTul <ZeraTttul@gmail.com>
2022-10-25 11:59:51 +0800

    10.25

--- lib/rgb.cpp
+++ lib/rgb.cpp
@@ -93,8 +93,6 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
 
         LUT(_src, BrightnessLut, _src);
 
-<<<<<<< HEAD
-=======
         //高斯滤波
         GaussianBlur( _src,
                       _src,
@@ -102,18 +100,10 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
                       0,
                       0 );
 
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
         //分离色彩通道
         cv::cvtColor(_src, _graySrc, cv::COLOR_BGR2GRAY);
 
         //高斯滤波
-<<<<<<< HEAD
-        GaussianBlur( _graySrc,
-                      _graySrc,
-                      Size(11, 11),
-                      0,
-                      0 );
-=======
         // GaussianBlur( _graySrc,
         //               _graySrc,
         //               Size(11, 11),
@@ -125,7 +115,6 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
         //直方图均质化 能把灰度图像几乎变回没灰度的时候
         // Mat _equalseparaSrc;
         // _equalseparaSrc = EqualHist(_graySrc);
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
 
         //灰度二值化
         cv::threshold(_graySrc,
@@ -139,18 +128,9 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
                      _splitSrc[2],
                      _separationSrc);
 
-<<<<<<< HEAD
-        Mat _grayseparaSrc;
-        cvtColor(_separationSrc, _grayseparaSrc, cv::COLOR_BGR2GRAY);
-        imshow("_grayseparasrc",_grayseparaSrc);
-        // Mat hist;
-        // hist = GetHist(_separationSrc);
-        // imshow("hist",hist);
-=======
         //Mat _grayseparaSrc;
         // equalizeHist(_grayseparaSrc, _equalseparaSrc);
         //imshow("_separationSrc", _separationSrc);
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
 
         //红蓝二值化
         cv::threshold(_separationSrc,
@@ -158,23 +138,15 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
                       _para.separationThreshold_BLUE,
                       255,
                       cv::THRESH_BINARY);
-<<<<<<< HEAD
-        //imshow("separationsrc",_separationSrc);
-=======
 
         //imshow("separationsrc",_separationSrc);
 
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
         cv::subtract(_splitSrc[0], _splitSrc[1], _separationSrcGreen);
         cv::erode(_separationSrcGreen, _separationSrcGreen, Rgb::structuringElement3());
         cv::dilate(_separationSrcGreen, _separationSrcGreen, Rgb::structuringElement5());
 
         //逻辑与获得最终二值化图像
-<<<<<<< HEAD
-        _maxColor = _separationSrc & _graySrc ;
-=======
         _maxColor = _separationSrc & _graySrc & _separationSrcGreen;
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
 
         //膨胀
         cv::dilate(_maxColor, _maxColor, Rgb::structuringElement5());
@@ -183,19 +155,6 @@ Mat Rgb::imagePreprocess(const cv::Mat &src, bool flag) {
     return _maxColor;
 }
 
-<<<<<<< HEAD
-Mat Rgb:: GetHist(Mat& image)
-{
-    Mat hist;
-    const int channels[] = {0};
-    int dims = 1;
-    int histSize[] = {256};
-    float granges[] = {0, 255};
-    const float *ranges[] = {granges};
-    calcHist(&image, 1, channels, Mat(), hist, dims, histSize, ranges);
-    equalizeHist(hist,hist);
-    return hist;
-=======
 Mat Rgb:: EqualHist(Mat image)
 {
     Mat equalImg;
@@ -203,5 +162,4 @@ Mat Rgb:: EqualHist(Mat image)
     equalizeHist(image, equalImg);
     imshow("equalImg", equalImg);
     return equalImg;
->>>>>>> bef8c8c906c207319740c4f400c857de90997db8
 }
